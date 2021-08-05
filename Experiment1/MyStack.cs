dcf30d783923a72836d30961b21e4d6f38953dfd
Paul Grebenc <paul@grebenc.ca>
2021-08-05 14:38:42 -0400

    Changes to build something else that is simple.

--- Experiment1/MyStack.cs
+++ Experiment1/MyStack.cs
@@ -1,17 +1,38 @@
 using Pulumi;
-using Pulumi.Aws.S3;
+using Pulumi.Aws.Ec2;
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        // Create an AWS resource (S3 Bucket)
-        var bucket = new Bucket("my-bucket");
+        var fooVpc = new Vpc("FooVPC", new VpcArgs()
+        {
+            CidrBlock = "10.0.0.0/16",
+            EnableDnsHostnames = true,
+            EnableDnsSupport = true
+        });
+        new Tag("FooVPCTag", new TagArgs()
+        {
+            Key = "Name",
+            Value = "FooVPC",
+            ResourceId = fooVpc.Id
+        });
 
-        // Export the name of the bucket
-        this.BucketName = bucket.Id;
+        FooVpcId = fooVpc.Id;
+
+
+        var fooInternetGateway = new InternetGateway("FooInternetGateway", new InternetGatewayArgs()
+        {
+            VpcId = fooVpc.Id
+        });
+        new Tag("FooInternetGatewayTag", new TagArgs()
+        {
+            Key = "Name",
+            Value = "FooInternetGateway",
+            ResourceId = fooInternetGateway.Id
+        });
     }
 
     [Output]
-    public Output<string> BucketName { get; set; }
+    public Output<string> FooVpcId { get; set; }
 }
